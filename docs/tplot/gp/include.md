d0c88639265705ac5e4d5224c9a4157b368485f6
Helfer Thomas <thomas.helfer@cea.fr>
2017-03-01 21:04:37 +0100

    add the structure for the documentation

---
+++ docs/tplot/gp/include.md
@@ -0,0 +1,8 @@
+The `include` keyword allows to include another file.
+
+## Example
+
+~~~~{.gnuplot}
+# include the 'cste.gp' file
+include 'cste.gp'
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ include/TFEL/Plot/Config/GetInstallPath.hxx
@@ -0,0 +1,25 @@
+/*!
+ * \file   GetInstallPath.hxx
+ * \brief    
+ * \author THOMAS HELFER
+ * \date   01 mars 2017
+ */
+
+#ifndef LIB_TFEL_PATH_GETINSTALLPATH_HXX
+#define LIB_TFEL_PATH_GETINSTALLPATH_HXX
+
+#include<string>
+#include"TFEL/Plot/Config.hxx"
+
+namespace tfel{
+
+  namespace plot{
+
+    TFELPLOT_VISIBILITY_EXPORT std::string
+    getInstallPath(void);
+
+  } // end of namespace plot
+    
+} // end of namespace tfel
+
+#endif /* LIB_TFEL_PATH_GETINSTALLPATH_HXX */
