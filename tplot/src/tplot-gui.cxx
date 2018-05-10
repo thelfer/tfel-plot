99e80843ed2f735b88b0df3ccb26e4642baac852
Thomas Helfer <tfel-contact@cea.fr>
2018-05-10 21:15:15 +0200

    small fix

--- tplot/src/tplot-gui.cxx
+++ tplot/src/tplot-gui.cxx
@@ -5,7 +5,6 @@
  * \date   21 jan 2008
  */
 
-<<<<<<< HEAD
 #include<QtCore/QtDebug>
 #include<QtWidgets/QMessageBox>
 #include<QtWidgets/QApplication>
@@ -38,31 +37,6 @@ namespace tfel
 	  //	  QMessageBox::critical(this,"TPlotApplication","unknown exception");
 	  abort();
 	}
-=======
-#include <QtCore/QtDebug>
-#include <QtWidgets/QMessageBox>
-#include <QtWidgets/QApplication>
-#include "TFEL/Plot/TPlot.hxx"
-
-namespace tfel {
-
-  namespace plot {
-
-    struct TPlotApplication : public QApplication {
-      TPlotApplication(int &c, char **v) : QApplication(c, v) {}
-
-      bool notify(QObject *rec, QEvent *ev) override {
-        try {
-          return QApplication::notify(rec, ev);
-        } catch (std::exception &e) {
-          QMessageBox::critical(nullptr, "TPlotApplication", e.what());
-          return false;
-        } catch (...) {
-          //	  QMessageBox::critical(this,"TPlotApplication","unknown
-          //exception");
-          abort();
-        }
->>>>>>> b30f13705f2f5dbecf436d7743f410b99de6ef2d
       }
     };  // end of struct TPlotApplication
 
@@ -70,14 +44,9 @@ namespace tfel {
 
 }  // end of namespace tfel
 
-<<<<<<< HEAD
 int main(int argc,char** argv)
 {
   tfel::plot::initRessources();
-=======
-int main(int argc, char **argv) {
-  Q_INIT_RESOURCE(TFELPlotResources);
->>>>>>> b30f13705f2f5dbecf436d7743f410b99de6ef2d
   tfel::plot::TPlotApplication a(argc, argv);
   try {
     tfel::plot::TPlot tplot(argc, argv);
